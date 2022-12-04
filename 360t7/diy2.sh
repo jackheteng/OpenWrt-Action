1acf484da373cc6d06c64d13284e8de3e861ea6a
QiYueYiya <qiyueyi@pm.me>
2022-12-04 15:57:05 +0800

    update

--- 360t7/diy2.sh
+++ 360t7/diy2.sh
@@ -13,8 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='Lenovo'/g" package/base-files/files/bin/config_generate
-target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
-for configFile in $(ls target/linux/mediatek/$target/config*)
+for configFile in $(ls target/linux/mediatek/mt7981/config*)
 do
     echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
 done
