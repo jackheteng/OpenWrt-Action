53171ae6e80c5def740f8624c8bca8f57d4bfde4
QiYueYi <qiyueyi@pm.me>
2022-12-08 11:17:28 +0800

    Update diy2.sh

--- x86_64/diy2.sh
+++ x86_64/diy2.sh
@@ -13,4 +13,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By QiYueYi@$(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
